e5fa1f621c954381faff101b277e3c3784b4334b
sumitgolu4uc <sumitgolu4uc@gmail.com>
2025-09-12 22:07:44 +0530

    Time: 169 ms (5.05%) | Memory: 70.1 MB (5.24%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     int countCharacters(vector<string>& words, string chars) {
-        int cnt = 0;
-
-        // frequency of available characters
-        vector<int> freq_chars(26, 0);
-        for(char c : chars) freq_chars[c - 'a']++;
-
-        for(string &s : words) {
-            vector<int> freq_word(26, 0);
-            bool flag = true;
-
-            for(char c : s) {
-                freq_word[c - 'a']++;
-                if (freq_word[c - 'a'] > freq_chars[c - 'a']) {
-                    flag = false;
-                    break;
-                }
+        int cnt=0;
+        map<char,int>m;
+        for(char c:chars) m[c]++;
+        for(int i=0;i<words.size();i++){
+            map<char,int>temp=m;
+            string s=words[i];
+            bool flag=true;
+            for(int i=0;i<s.size();i++){
+                if(temp[s[i]]>0) temp[s[i]]--; 
+                else flag=false;
             }
+            if(flag==true) cnt+=s.size();
 
-            if(flag) cnt += s.size();
         }
-
         return cnt;
     }
 };
