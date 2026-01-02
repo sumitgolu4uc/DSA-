f890b758d22021b5d6db6a01668a92d7c41b273f
sumitgolu4uc <sumitgolu4uc@gmail.com>
2026-01-02 09:23:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (39.12%) - LeetSync

---
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& position) {
+        int n=position.size();
+        int even=0,odd=0;
+       for(int x:position ){
+        if(x%2==0) even++;
+        else odd++;
+       }
+        return min(odd,even);
+    }
+};
